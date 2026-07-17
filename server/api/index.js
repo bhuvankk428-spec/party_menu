8d51a32b64587d6f90a257c9ca8a4e432c697d12
bhuvankk428-spec <bhuvankk428@gmail.com>
2026-07-18 04:01:18 +0530

    ini

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,30 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+
+import connectDB from "../config/db.js";
 import authRoutes from "../routes/authRoutes.js";
+import menuRoutes from "../routes/menuRoutes.js";
+
+dotenv.config();
 
 const app = express();
 
+await connectDB();
+
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/api/auth", authRoutes);
+app.use("/api/menu", menuRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Party Menu API is running",
+  });
+});
 
 export default app;
