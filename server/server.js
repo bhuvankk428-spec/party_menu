cdedff512c1abc047b0dbf0b64e6d5c420ee4e2b
bhuvankk428-spec <bhuvankk428@gmail.com>
2026-07-18 03:44:18 +0530

    Configure backend for Vercel

--- server/server.js
+++ server/server.js
@@ -27,9 +27,3 @@ app.get("/", (req, res) => {
     message: "Party Menu API is running",
   });
 });
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
